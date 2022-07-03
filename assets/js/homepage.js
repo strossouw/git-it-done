0c0d8da0bc8ff278372a6acd3ccca02aa2a8111b
SRoss <srossouw@uwm.edu>
2022-07-03 15:13:25 -0500

    added additional page

---
+++ assets/js/homepage.js
@@ -0,0 +1,27 @@
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+  
+  var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+  };
+
+    userFormEl.addEventListener("submit", formSubmitHandler);
+  
+
+
